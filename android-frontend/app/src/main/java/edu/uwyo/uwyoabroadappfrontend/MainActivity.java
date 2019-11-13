fcf623e6b870e5708453a56667e9ddd4242cd01f
Samuel Blair <31827076+sblair3@users.noreply.github.com>
2019-11-12 18:58:29 -0700

    Worked on the Outgoing home page

--- android-frontend/app/src/main/java/edu/uwyo/uwyoabroadappfrontend/MainActivity.java
+++ android-frontend/app/src/main/java/edu/uwyo/uwyoabroadappfrontend/MainActivity.java
@@ -6,15 +6,15 @@ import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .add(R.id.container, new IncomingHomePage())
+                .add(R.id.container, new StartUpPage())
                 .commit();
     }
 }
