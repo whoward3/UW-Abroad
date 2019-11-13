516f6f9b9d9a15dbe040d44127106e7a3086d555
Samuel Blair <31827076+sblair3@users.noreply.github.com>
2019-11-12 17:05:13 -0700

    Added Incoming homepage.java and xml

---
+++ android-frontend/app/src/main/java/edu/uwyo/uwyoabroadappfrontend/IncomingHomePage.java
@@ -0,0 +1,31 @@
+package edu.uwyo.uwyoabroadappfrontend;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class IncomingHomePage extends Fragment {
+
+
+    public IncomingHomePage() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_incoming_home_page, container, false);
+    }
+
+}
