525f4ae8e5873d65c76697d1ca99e2a75707eb2a
Ethan J McGarvey <emcgarve@uwyo.edu>
2020-03-02 15:33:44 -0700

    WH: Production Build 1

--- azure-backend/SapWebAppService/Properties/AssemblyInfo.cs
+++ azure-backend/SapWebAppService/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SapWebApp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SapWebAppService")]
+[assembly: AssemblyDescription("The Backend Service for the SapWebApp")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SAP")]
 [assembly: AssemblyProduct("SapWebApp")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
