68b1698ee2fcd7c5c3114ac6d76c3885a3e60214
Alice Blair <aliceITR@outlook.com>
2020-03-09 21:30:56 -0600

    Updated A few things

--- zumo-integrated-concept/app/src/main/java/com/example/sapApp/SchoolHTMLviewer.java
+++ zumo-integrated-concept/app/src/main/java/com/example/sapApp/SchoolHTMLviewer.java
@@ -1,9 +1,5 @@
 package com.example.sapApp;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
