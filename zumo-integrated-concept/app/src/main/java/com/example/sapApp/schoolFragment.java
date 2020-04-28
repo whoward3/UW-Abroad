e238eb45c0de6d7203388a86e1ffff01379816f9
Chance Sweetser <chance.sweetser@outlook.com>
2020-04-28 16:12:51 -0600

    added disclaimer to areas of study

--- zumo-integrated-concept/app/src/main/java/com/example/sapApp/schoolFragment.java
+++ zumo-integrated-concept/app/src/main/java/com/example/sapApp/schoolFragment.java
@@ -88,7 +88,7 @@ public class schoolFragment extends Fragment {
     @Override
     public View  onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.listview_for_cardview, container, false);
+        View view = inflater.inflate(R.layout.listview_for_abroadview, container, false);
 
         try {
             // Create the client instance, using the provided mobile app URL.
